04b577b1a8909e609cf34ddc38e06bfe7b779cdd
Alecs <erick.borges@outlook.es>
2026-07-13 14:37:43 -0600

    fix: use dsn for jenssegers/mongodb + printf for .env creation

--- backend/config/database.php
+++ backend/config/database.php
@@ -116,7 +116,7 @@ return [
 
         'mongodb' => [
             'driver'   => 'mongodb',
-            'uri'      => env('MONGODB_URI'),
+            'dsn'      => env('MONGODB_URI'),
             'database' => env('MONGODB_DATABASE', 'tap_terminal'),
         ],
 
